1682d46d0bfe7f0f8ec7b1082e34b225b581fb82
milax <jenyok@bk.ru>
2012-03-12 16:20:47 +0200

    minor fixes/updates

--- js/carousel.js
+++ js/carousel.js
@@ -1,179 +1,180 @@
-if (!Nets) var Nets = {};
+/*
 
-(function($){
+jQuery Milax Carousel
+jQuery Plugin
 
-	$(function(){
+Author: Eugene Kuzmin
+Copyright: 2012, Eugene Kuzmin
 
-		new Nets.Carousel('#topCarousel');
+-----------------------------------------------------------------------*/
+(function ($) {
+
+    $.fn.mxCarousel = function (settings) {
+
+        // options
+        var options = jQuery.extend({
+            errorMsgClass: '.mxError',
+            errorFieldClass: '.mxNotValidated',
+            fieldsToValidateSelector: '.mxValidate',
+            btnSubmitSelector: '.btnSubmit',
+            fieldHolderSelector: 'dd',
+            isValidFunc: false
+        }, settings);
+
+        var carouselWrappers = this;
+
+        this.init = function() {
+            carouselWrappers.each(function () {
+                CarouselWork($(this));
+            });
+        }
 
 
-		$('.btn.btnArrow').each(function(){
-            var self = $(this);
-            var btn = self.find('.button');
-            var tagName = btn[0].nodeName.toLowerCase();
+        var CarouselWork = function (currCarousel) {
+            var that = this;
+            var slides = {
+                    holder: currCarousel.find('ul.slider', that)
+                },
+                nav = {
+                    holder: currCarousel.find('.pagination', that)
+                },
+                rotation = {
+                    moveWidth: 0,
+                    isScrolling: false,
+                    actionSpeed: 500
+                };
 
-            if(tagName == 'span' || tagName == 'button') {
-                btn.html('<span class="btnTextHolder">' + $.trim(btn.text()) + '</span>')  
+            slides.items = slides.holder.find('>li', that);
+            slides.quantity = slides.items.length;
+
+            nav.links = nav.holder.find('a', that);
+            nav.btnNext = nav.holder.find('a.next', that);
+            nav.btnPrev = nav.holder.find('a.prev', that);
+
+            /* if no slides - just exit */
+            if (!slides.quantity) {
+                return false;
             }
-            else {
-                btn.wrap('<span class="btnHolder" />');
+
+            /* click on navigation links handler */
+            nav.holder.on('click', 'a', function(){
+                var self = $(this);
+
+                if(self.hasClass('current')) {
+                    return false;
+                }
+                else if(self.hasClass('next')) {
+                    go2Next();  
+                }
+                else if (self.hasClass('prev')) {
+                    go2Prev();
+                }
+                else {
+                    go2Slide(nav.links.index(self));
+                }
+
+                return false;
+            });
+
+            /* initial set sizes for the carousel */
+            setSizes();
+
+            /* if user resizes a browser window then reinit carousel's sizes */
+            $(window).resize(function(){
+                setSizes();
+            });
+
+
+            /* function - set/change status of navigation links */
+            function setNav(i) {
+                if(typeof(i) == 'undefined') {
+                    i = slides.holder.find('li:first').data('index');
+                }
+                nav.links.removeClass('current');
+                nav.links.eq(i).addClass('current');
             }
 
-        });
+            /* function - go to @i slide */
+            function go2Slide(i) {
+                var indToShow = i,
+                    currInd = nav.links.index(nav.holder.find('a.current')),
+                    actualSpeed = rotation.actionSpeed/Math.abs(indToShow - currInd);
+
+                if(indToShow > currInd) {
+                    go2Next(true, indToShow, actualSpeed);
+                }
+                else {
+                    go2Prev(true, indToShow, actualSpeed);
+                }
+            }
+
+            
+            function go2Next(cycle, indToShow, actualSpeed){
+                if (rotation.isScrolling) {
+                    return false;
+                }
+                rotation.isScrolling = true;
+                cycle = typeof(cycle) == 'undefined' ? false : cycle; 
+                actualSpeed = typeof(actualSpeed) == 'undefined' ? rotation.actionSpeed : actualSpeed;
+
+                var currLeftPos = slides.holder.css('left').replace("px", "");
+                currLeftPos = currLeftPos == 'auto' ? 0 : currLeftPos;
+
+                slides.holder.animate({ 'left': "-=" + rotation.moveWidth + 'px' }, actualSpeed, function () {
+                    slides.holder.find('li:first').clone().appendTo(slides.holder);
+                    slides.holder.find('li:first').remove();
+                    slides.holder.css('left', 0);
+                    rotation.isScrolling = false;
+                    setNav();
+                    if(cycle) {
+                        var currInd = nav.links.index(nav.holder.find('a.current')); 
+                        if(indToShow != currInd) {
+                            go2Next(true, indToShow);
+                        }
+                    }
+                });
+            }
+
+            function go2Prev(cycle, indToShow, actualSpeed) {
+                if (rotation.isScrolling) {
+                    return false;
+                }
+                rotation.isScrolling = true;
+                cycle = typeof(cycle) == 'undefined' ? false : cycle; 
+                actualSpeed = typeof(actualSpeed) == 'undefined' ? rotation.actionSpeed : actualSpeed;
+
+                var currLeftPos = slides.holder.css('left').replace("px", "");
+                currLeftPos = currLeftPos == 'auto' ? 0 : currLeftPos;
+
+                slides.holder.css('left', (currLeftPos - rotation.moveWidth) + 'px');
+                slides.holder.find('li:last').clone().prependTo(slides.holder);
+                slides.holder.find('li:last').remove();
+                slides.holder.animate( { 'left': '+=' + rotation.moveWidth + 'px' }, actualSpeed, function () {
+                    rotation.isScrolling = false;
+                    setNav();
+                    if(cycle) {
+                        var currInd = nav.links.index(nav.holder.find('a.current')); 
+                        if(indToShow != currInd) {
+                            go2Prev(true, indToShow);
+                        }
+                    }
+                } );
+                
+            }
+
+            function setSizes() {
+                rotation.moveWidth = parseInt($('.subTopCarousel').width());
+                slides.items = slides.holder.find('>li', that);
+
+                slides.holder.width(rotation.moveWidth * (slides.quantity + 1));
+                slides.items.width(rotation.moveWidth);
+                nav.holder.width(rotation.moveWidth);
+            }
+
+        }
+
+        this.init();
 
 
-	})
-
-
-	// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	// TODO: the first parameter is selector to find box with carousel functionality
-	Nets.Carousel = function (selector)	{
-
-		var carouselWrappers = $(selector);
-
-	    this.Carousel = function () {
-	    	// init
-	        carouselWrappers.each(function () {
-	            CarouselWork($(this));
-	        });
-	    }
-
-	    var CarouselWork = function (currCarousel) {
-	    	var that = this;
-	        var carouselUL = currCarousel.find('ul.slider', that),
-	        	carouselLI = carouselUL.find('>li', that),
-	        	pagination = currCarousel.find('.pagination'),
-	        	navLinks = pagination.find('a'),
-	        	btnNext = pagination.find('a.next', that),
-	        	btnPrev = pagination.find('a.prev', that),
-	        	steps = 1,
-	        	moveWidth,
-	        	isScrolling = false,
-	        	actionSpeed = 500;
-
-	        if (!carouselLI.length) {
-	            return false;
-	        }
-
-			pagination.on('click', 'a', function(){
-				var self = $(this);
-
-				if(self.hasClass('current')) {
-					return false;
-				}
-				else if(self.hasClass('next')) {
-					go2Next();	
-				}
-				else if (self.hasClass('prev')) {
-					go2Prev();
-				}
-				else {
-					go2Slide(navLinks.index(self));
-				}
-				return false;
-			});
-
-
-	        setSizes();
-
-	        $(window).resize(function(){
-	        	setSizes();
-	        });
-
-	        function setPagination(i) {
-	        	if(typeof(i) == 'undefined') {
-	        		i = carouselUL.find('li:first').data('index');
-	        	}
-	        	navLinks.removeClass('current');
-	        	navLinks.eq(i).addClass('current');
-	        }
-
-	        function go2Slide(i) {
-	        	var indToShow = i;
-	        	var currInd = navLinks.index(pagination.find('a.current'));
-	        	var actualSpeed = actionSpeed/Math.abs(indToShow - currInd);
-	        	if(indToShow > currInd) {
-        			go2Next(true, indToShow, actualSpeed);
-	        	}
-	        	else {
-	        		go2Prev(true, indToShow, actualSpeed);	
-	        	}
-	        }
-
-	        function go2Next(cycle, indToShow, actualSpeed){
-	            if (isScrolling) {
-	                return false;
-	            }
-	            isScrolling = true;
-	            cycle = typeof(cycle) == 'undefined' ? false : cycle; 
-	            actualSpeed = typeof(actualSpeed) == 'undefined' ? actionSpeed : actualSpeed;
-
-	            var currLeftPos = carouselUL.css('left').replace("px", "");
-	            currLeftPos = currLeftPos == 'auto' ? 0 : currLeftPos;
-
- 	            carouselUL.animate({ 'left': "-=" + moveWidth + 'px' }, actualSpeed, function () {
-                    carouselUL.find('li:first').clone().appendTo(carouselUL);
-                    carouselUL.find('li:first').remove();
-	                carouselUL.css('left', 0);
-	                isScrolling = false;
-	                setPagination();
-	                if(cycle) {
-	                	var currInd = navLinks.index(pagination.find('a.current'));	
-	                	if(indToShow != currInd) {
-	                		go2Next(true, indToShow);
-	                	}
-	                }
-	            });
-	        }
-
-	        function go2Prev(cycle, indToShow, actualSpeed) {
-	            if (isScrolling) {
-	                return false;
-	            }
-	            isScrolling = true;
-	            cycle = typeof(cycle) == 'undefined' ? false : cycle; 
-				actualSpeed = typeof(actualSpeed) == 'undefined' ? actionSpeed : actualSpeed;
-
-	            var currLeftPos = carouselUL.css('left').replace("px", "");
-				currLeftPos = currLeftPos == 'auto' ? 0 : currLeftPos;
-
-	            carouselUL.css('left', (currLeftPos - moveWidth) + 'px');
-                carouselUL.find('li:last').clone().prependTo(carouselUL);
-                carouselUL.find('li:last').remove();
-	            carouselUL.animate( { 'left': '+=' + moveWidth + 'px' }, actualSpeed, function () {
-	            	isScrolling = false;
-	            	setPagination();
-	                if(cycle) {
-	                	var currInd = navLinks.index(pagination.find('a.current'));	
-	                	if(indToShow != currInd) {
-	                		go2Prev(true, indToShow);
-	                	}
-	                }	            	
-	            } );
-	            
-	        }
-
-			function setSizes() {
-		        var singleBoxWidth = $('.subTopCarousel').width(),
-		        	stepWidth = parseInt(singleBoxWidth);
-		        	
-		        moveWidth = steps * stepWidth;
-
-	        	var carouselLI = carouselUL.find('>li', that);
-
-		        carouselUL.width(parseInt(singleBoxWidth) * (carouselLI.length + parseInt(steps)));
-				carouselLI.width(parseInt(singleBoxWidth));
-				pagination.width(parseInt(singleBoxWidth));
-
-			}
-
-	    }
-
-
-	    this.Carousel();
-	}
-
-
-
-
-}(jQuery))
+    };
+})(jQuery);
